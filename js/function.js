fbe5c2386004f369a6cf1ad43adcaf17e91020bd
YumiN <yuminishi0309@gmail.com>
2023-10-24 23:40:40 +0900

    add: fadein-menu in SP/Tab

--- js/function.js
+++ js/function.js
@@ -1,9 +1,16 @@
-$(function() {
-// ここにjQueryを記述
+// ハンバーガーメニューボタン
+const menuBtn = document.querySelector('#btn');
+const btnTop = document.querySelector('#btn__top');
+const btnMiddle = document.querySelector('#btn__middle');
+const btnBottom = document.querySelector('#btn__bottom');
+const gnav = document.querySelector('#gnav');
+console.log(gnav);
 
-
-
-
-
-
+// メニューボタン操作時
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('btnBorderHide');
+    btnTop.classList.toggle('rotateTop');
+    btnMiddle.classList.toggle('hideMiddle');
+    btnBottom.classList.toggle('rotateBottom');
+    gnav.classList.toggle('gnavFadeIn');
 });
